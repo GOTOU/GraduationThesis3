d2c895b19eb9239756fb86e6ae434d5fc06937c4
GOTOU <satoshi31093@gmail.com>
2016-12-25 18:25:37 +0900

    add file

--- GraduationThesis4/app/src/main/java/com/example/koba/graduationthesis4/MainActivity.java
+++ GraduationThesis4/app/src/main/java/com/example/koba/graduationthesis4/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.koba.graduationthesis4;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.transition.Slide;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.KeyEvent;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.view.inputmethod.InputMethodManager;
-
+import java.lang.String;
 
 /*import org.jsoup.*;
 import org.jsoup.nodes.Document;
@@ -26,13 +28,14 @@ import org.jsoup.select.Elements;
 
 
 
-public class MainActivity extends AppCompatActivity implements SearchView.OnQueryChangeListener{
+public class MainActivity extends AppCompatActivity {
 
     WebView myWebView;
     EditText editText;
-    String str;
+    final String str = "http";
     String searchText;
     ProgressBar progressBar;
+    private SearchView searchView;
 
 
 
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
 
         myWebView = (WebView)findViewById(R.id.webview);
@@ -65,37 +69,84 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
-        //SearchViewを取得する
-        SearchView search = (SearchView)findViewById(R.id.searchView);
-//        MenuItem searchItem = menu.findItem(R.id.searchView);
-//        searchItem =
-        final SearchView searchView = (SearchView)searchItem.getActionView();
-        searchView.setOnQueryTextListener(this);
+        //SearchViewを取得するSearchView searchView= (SearchView)findViewById(R.id.searchView);
+        MenuItem menuItem = menu.findItem(R.id.searchView);
 
+        //SearchViewを取得する
+        searchView = (SearchView)MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                searchView.clearFocus();
+                if (query.startsWith(str)){
+                    myWebView.loadUrl(query);
+                } else {
+                    myWebView.loadUrl("https://www.google.co.jp/#q="+ query);
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
 
         return true;
     }
 
     @Override
-    public boolean onQueryTextChange(String newText){
-        return false;
-    }
-
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item) {  //アクションバーのメニューの処理
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                //設定の処理
+                break;
+
+            case R.id.reload:
+                myWebView.reload();
+                break;
+
+            case R.id.page_back:
+                if (myWebView.canGoBack()){
+                    myWebView.goBack();
+                }
+
+                break;
+
+            case R.id.page_progress:
+                if (myWebView.canGoForward()){
+                    myWebView.goForward();
+                }
+
+                break;
+
+            case R.id.new_tab:
+
+                break;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+
+        } else {
+            finish();
+        }
+
+        return false;
+
+    }
+
+
 
 }
